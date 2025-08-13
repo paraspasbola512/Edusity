d97aeae09d055d3aefa75d714a79e65e6b6bfaa1
paraspasbola512 <paraspasbola8@gmail.com>
2025-08-13 17:35:30 +0530

    Navbar: dynamic theme (dark/light) ( via useEffect & useState , ) Added testimonial section Integrated Contact Us (Web3Forms)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Programs/Programs";
 import Title from "./Components/Title/Title";
 import About from "./Components/About/About";
+import Campus from "./Components/Campus/Campus";
+import Testimonials from "./Components/Testimonials/Testimonials";
+import Contact from "./Components/Contact/Contact";
 
 const App = () => {
   return (
@@ -14,6 +17,12 @@ const App = () => {
         <Title subTitle="OUR PROGRAM" title="What We Offer" />
         <Programs />
         <About />
+        <Title subTitle="Gallery" title="Campus photos" />
+        <Campus />
+        <Title subTitle="TESTIMONIALS" title="What students says" />
+        <Testimonials />
+        <Title subTitle="CONTACT US" title="Get in Touch" />
+        <Contact />
       </div>
     </div>
   );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Navbar.css';
 import logo from '../../assets/logo.png';
 const Navbar = () => {
+    const [sticky, setSticky] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', ()=>{
+            window.scrollY  > 50 ? setSticky(true) : setSticky(false);
+        })
+    },[])
+    
     return (
-        <nav className='container dark-nav'>
+        <nav className= {`container ${sticky ? 'dark-nav' : ''}`}>
             <img src={logo} alt="" className='logo' />
             <ul>
                 <li>Home</li>
